c1c32327ac838364d32fed807310fda391aa8f74
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-02-03 17:27:42 +0300

    .

--- vk/v41/vkgeo.js
+++ vk/v41/vkgeo.js
@@ -327,7 +327,7 @@ function runPeriodicUpdate() {
                                 "code": code,
                                 "v":    VK_API_V
                             }, function(data) {
-                                console.log("DEBUG: " + data.toString());
+                                console.log("DEBUG: " + JSON.stringify(data));
                                 if (data.hasOwnProperty("response")) {
                                     if (data.response) {
                                         for (let i = 0; i < data.response.length; i++) {
