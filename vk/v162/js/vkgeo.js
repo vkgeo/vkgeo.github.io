75a3bb34af11c7b705b25c96890a193f374c2b6f
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-11-01 14:19:19 +0300

    .

--- vk/v162/js/vkgeo.js
+++ vk/v162/js/vkgeo.js
@@ -51,7 +51,7 @@ let VKGeo = (function() {
 
                 context.save();
 
-                context.scale(pixel_ratio, pixel_ratio);
+                context.scale(device_pixel_ratio, device_pixel_ratio);
 
                 context.save();
 
@@ -75,14 +75,12 @@ let VKGeo = (function() {
             }
         }
 
-        let pixel_ratio = window.devicePixelRatio > MIN_DEVICE_PIXEL_RATIO ? window.devicePixelRatio : MIN_DEVICE_PIXEL_RATIO;
-
         let canvas = document.createElement("canvas");
         let image  = null;
         let label  = null;
 
-        canvas.width           = size.width  * pixel_ratio;
-        canvas.height          = size.height * pixel_ratio;
+        canvas.width           = size.width  * device_pixel_ratio;
+        canvas.height          = size.height * device_pixel_ratio;
         canvas.className       = "controlPanelImage";
         canvas.style.width     = size.width  + "px";
         canvas.style.height    = size.height + "px";
@@ -168,7 +166,7 @@ let VKGeo = (function() {
 
                         context.save();
 
-                        context.scale(pixel_ratio, pixel_ratio);
+                        context.scale(device_pixel_ratio, device_pixel_ratio);
 
                         context.save();
 
@@ -194,14 +192,12 @@ let VKGeo = (function() {
                     }
                 }
 
-                let pixel_ratio = window.devicePixelRatio > MIN_DEVICE_PIXEL_RATIO ? window.devicePixelRatio : MIN_DEVICE_PIXEL_RATIO;
-
                 let canvas = document.createElement("canvas");
                 let image  = null;
                 let label  = null;
 
-                canvas.width  = size.width  * pixel_ratio;
-                canvas.height = size.height * pixel_ratio;
+                canvas.width  = size.width  * device_pixel_ratio;
+                canvas.height = size.height * device_pixel_ratio;
 
                 image = document.createElement("img");
 
@@ -224,8 +220,8 @@ let VKGeo = (function() {
 
                 return canvas;
             })(),
-            "imgSize": [size.width * pixel_ratio, size.height * pixel_ratio],
-            "scale":   1.0 / pixel_ratio
+            "imgSize": [size.width * device_pixel_ratio, size.height * device_pixel_ratio],
+            "scale":   1.0 / device_pixel_ratio
         });
     }
 
@@ -573,11 +569,12 @@ let VKGeo = (function() {
         });
     }
 
-    let map_was_touched  = false;
-    let my_photo_100     = DEFAULT_PHOTO_100_URL;
-    let my_marker        = null;
-    let tracked_marker   = null;
-    let vk_request_queue = [];
+    let device_pixel_ratio = window.devicePixelRatio > MIN_DEVICE_PIXEL_RATIO ? window.devicePixelRatio : MIN_DEVICE_PIXEL_RATIO;
+    let map_was_touched    = false;
+    let my_photo_100       = DEFAULT_PHOTO_100_URL;
+    let my_marker          = null;
+    let tracked_marker     = null;
+    let vk_request_queue   = [];
 
     let marker_source = new ol.source.Vector({
         "features": []
