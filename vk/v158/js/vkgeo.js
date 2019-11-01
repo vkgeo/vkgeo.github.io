b44d5fa48c7e3d5250ab2227d68fdae7c6abc283
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-11-01 04:51:50 +0300

    .

--- vk/v158/js/vkgeo.js
+++ vk/v158/js/vkgeo.js
@@ -202,8 +202,8 @@ let VKGeo = (function() {
 
                 canvas.width        = size.width  * pixel_ratio;
                 canvas.height       = size.height * pixel_ratio;
-                //canvas.style.width  = size.width  + "px";
-                //canvas.style.height = size.height + "px";
+                canvas.style.width  = size.width  + "px";
+                canvas.style.height = size.height + "px";
 
                 image = document.createElement("img");
 
@@ -226,6 +226,7 @@ let VKGeo = (function() {
 
                 return canvas;
             })(),
+            "size":    [size.width, size.height],
             "imgSize": [size.width, size.height]
         });
     }
